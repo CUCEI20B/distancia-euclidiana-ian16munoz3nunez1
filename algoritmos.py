90547433e88d14ffee3a7189e08819d8394c70f8
ian16munoz3nune <70918281+ian16munoz3nunez1@users.noreply.github.com>
2020-10-23 19:45:57 +0000

    se agrego la función distancia_euclidiana

--- algoritmos.py
+++ algoritmos.py
@@ -3,12 +3,3 @@ import math
 def distancia_euclidiana(x_1=0.0, y_1=0.0, x_2=0.0, y_2=0.0):
   distancia = math.sqrt(((x_2-x_1)**2)+((y_2-y_1)**2))
   return distancia
-
-x_1 = float(input("Ingresa x1: "))
-y_1 = float(input("Ingresa y1: "))
-x_2 = float(input("Ingresa x2: "))
-y_2 = float(input("Ingresa y2: "))
-
-d = distancia_euclidiana(x_1, y_1, x_2, y_2)
-
-print("La distancia es", d)
